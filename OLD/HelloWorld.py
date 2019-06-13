f265f08cc7e6c4bae0349108ab9072f3066af6c9
Man-Hyuk,Han <lifeofhyuk@gmail.com>
2019-06-13 18:03:53 +0900

    delete backup files
    They do not need from now

--- OLD/HelloWorld.py
+++
@@ -1,3 +0,0 @@
-print("Hello World !! from Bitbucket")
-print("결과값은", 2*7, "입니다.")
-print("Welcome World !! from Local")
--- OLD/function_default.py
+++
@@ -1,7 +0,0 @@
-def say(message, times=1):
-
-    print(message * times)
-
-say('*Hello')
-say('#Hello\n', 5)
-
